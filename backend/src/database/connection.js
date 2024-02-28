ab0dc90a8c7d4e786926cf8aed8aabff0a1017bb
Gabriel Istailer <gabriel.istailer@hotmail.com>
2024-02-28 18:55:53 -0300

    commit initial

---
+++ backend/src/database/connection.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+var knex = require('knex')({
+    client: "mysql",
+    connection: {
+        host: process.env.HOST_DB,
+        user: process.env.USER_DB,
+        password: process.env.PASSWORD_DB,
+        database: process.env.DATABASE
+    }
+});
+
+module.exports = knex;
